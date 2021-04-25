abef055d8bf33cc8e29f3659eb1e6833139d7786
Rubens <rubao.ma@gmail.com>
2021-04-25 16:23:30 -0300

    implemented push notification firebase

--- app/src/main/java/com/rubdev/applicationnotification/MainActivity.kt
+++ app/src/main/java/com/rubdev/applicationnotification/MainActivity.kt
@@ -2,7 +2,9 @@ package com.rubdev.applicationnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         btnSend = findViewById(R.id.button_send_notification)
         btnSend.setOnClickListener {
-
+            this.showNotification("1234", "BootCamp Android", "Kotlin Android curso")
         }
+
+        Log.i("**newToken", FirebaseInstanceId.getInstance().token.toString())
+
     }
 }
---
+++ app/src/main/java/com/rubdev/applicationnotification/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,15 @@
+package com.rubdev.applicationnotification
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class MyFirebaseInstanceIdService : FirebaseInstanceIdService() {
+
+    override fun onTokenRefresh() {
+        Log.i("**newTokenService", FirebaseInstanceId.getInstance().token.toString())
+    }
+}
